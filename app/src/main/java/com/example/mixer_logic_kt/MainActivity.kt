1f0bd64b545b1eb668693d0fa1344b937f9800b7
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-04-22 00:53:46 +0300

    bottom navigation added but shows also in the wrong screens

--- app/src/main/java/com/example/mixer_logic_kt/MainActivity.kt
+++ app/src/main/java/com/example/mixer_logic_kt/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.mixer_logic_kt.databinding.ActivityMainBinding
 
@@ -22,6 +23,9 @@ class MainActivity : AppCompatActivity() {
 
         //This ensures action bar (app bar) buttons, like the menu option in LetterListFragment are visible.
         setupActionBarWithNavController(navController)
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
