0f2cf810fab25a34fc1005c1157f6a582e7724af
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-04-21 20:06:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mixer_logic_kt/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mixer_logic_kt
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
