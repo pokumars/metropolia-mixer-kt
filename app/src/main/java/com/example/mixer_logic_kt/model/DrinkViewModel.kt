5c03e37a965353001d7c00f6fd04c066a4ebe6b9
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-05-10 22:25:41 +0300

    create a shared viewModel

---
+++ app/src/main/java/com/example/mixer_logic_kt/model/DrinkViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.mixer_logic_kt.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.mixer_logic_kt.testDataSource.SomeDrinks
+
+class DrinkViewModel: ViewModel() {
+    private val localDrinks = SomeDrinks().loadDrinks()
+    private val _drinks = MutableLiveData<List<Drink>>()
+    val drinks: LiveData<List<Drink>> = _drinks
+
+    private val _favourites =  MutableLiveData<List<Drink>>()
+    val favourites: LiveData<List<Drink>> = _favourites
+
+    /*private val _user = MutableLiveData<>()
+    val user: LiveData<> = _user*/
+
+    fun setDrinks() {
+        _drinks.value = localDrinks
+    }
+}
