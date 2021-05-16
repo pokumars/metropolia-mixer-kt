d96df5a6c5d39d66af6a67c918bb3d25372e84b2
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-05-16 18:44:25 +0300

    add app icon; add commented-out real endpoint

--- app/src/main/java/com/example/mixer_logic_kt/network/DrinksApiService.kt
+++ app/src/main/java/com/example/mixer_logic_kt/network/DrinksApiService.kt
@@ -8,7 +8,8 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.*
 
 
-private const val BASE_URL = "http://10.0.2.2:3003/api/"
+ private const val BASE_URL = "http://10.0.2.2:3003/api/"
+//private const val BASE_URL = "https://mixerlogic.herokuapp.com/api/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
