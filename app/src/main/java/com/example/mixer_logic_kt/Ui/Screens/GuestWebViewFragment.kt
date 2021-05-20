5034268606b147f24802870cb898bd78165b6573
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-05-20 16:57:22 +0300

    remove boilerplate code

--- app/src/main/java/com/example/mixer_logic_kt/Ui/Screens/GuestWebViewFragment.kt
+++ app/src/main/java/com/example/mixer_logic_kt/Ui/Screens/GuestWebViewFragment.kt
@@ -10,16 +10,6 @@ import androidx.fragment.app.Fragment
 import com.example.mixer_logic_kt.databinding.FragmentGuestWebViewBinding
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GuestWebViewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GuestWebViewFragment : Fragment() {
     private var _binding: FragmentGuestWebViewBinding? = null
     private val binding get() =_binding!!
