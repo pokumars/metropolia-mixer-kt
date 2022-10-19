d62c300aae6703a37a1db61c1a6cc1c85e592382
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2022-10-19 15:14:42 +0300

    change backend url link to fly.io from heroku

--- app/src/main/java/com/example/mixer_logic_kt/network/DrinksApiService.kt
+++ app/src/main/java/com/example/mixer_logic_kt/network/DrinksApiService.kt
@@ -8,8 +8,8 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.*
 
 
-//private const val BASE_URL = "http://10.0.2.2:3003/api/"
-private const val BASE_URL = "https://metropolia-mixer.herokuapp.com/api/"
+//private const val BASE_URL = "http://10.0.2.2:3003/api/" "https://metropolia-mixer.herokuapp.com/api/" "https://metropolia-mixer-backend.fly.dev"
+private const val BASE_URL =   "https://metropolia-mixer-backend.fly.dev/api/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
